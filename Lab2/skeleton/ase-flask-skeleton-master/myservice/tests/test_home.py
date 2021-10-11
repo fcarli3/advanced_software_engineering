0dfcf26a08d1a7f2e89d710b2cdfd5fd7a2e7762
Francesco Carli <62562180+fcarli3@users.noreply.github.com>
2021-10-11 08:19:35 +0200

    Delete Lab2/skeleton/ase-flask-skeleton-master directory

--- Lab2/skeleton/ase-flask-skeleton-master/myservice/tests/test_home.py
+++
@@ -1,7 +0,0 @@
-import unittest
-
-
-class TestSomething(unittest.TestCase):
-
-    def test_my_view(self):
-        pass
--- Lab2/skeleton/ase-flask-skeleton-master/myservice/views/__init__.py
+++
@@ -1,5 +0,0 @@
-from myservice.views.home import home
-from myservice.views.calc import calc
-
-# Declare all blueprints here :)
-blueprints = [home, calc]
